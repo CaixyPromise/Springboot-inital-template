ff6e877586fae9a3676c872b9d23fa0318c2f5b7
CaixyPromise <caixypromised@gmail.com>
2024-07-15 19:42:06 +0800

    style: 修改banner和杂项修改

--- src/main/java/com/caixy/adminSystem/MainApplication.java
+++ src/main/java/com/caixy/adminSystem/MainApplication.java
@@ -16,7 +16,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
 public class MainApplication
 {
-
     public static void main(String[] args)
     {
         SpringApplication.run(MainApplication.class, args);
