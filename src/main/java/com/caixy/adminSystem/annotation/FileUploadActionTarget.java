e8c7b58e58c1dffe893c2c1f5c1c360c7b3d1ff1
CaixyPromise <caixypromised@gmail.com>
2024-07-18 01:58:40 +0800

    fix: 补充修复上传业务注解解耦操作

---
+++ src/main/java/com/caixy/adminSystem/annotation/FileUploadActionTarget.java
@@ -0,0 +1,22 @@
+package com.caixy.adminSystem.annotation;
+
+import com.caixy.adminSystem.model.enums.FileActionBizEnum;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 文件上传业务处理器
+ *
+ * @author CAIXYPROMISE
+ * @name com.caixy.adminSystem.annotation.FileUploadActionTarget
+ * @since 2024-07-18 01:39
+ **/
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface FileUploadActionTarget
+{
+    FileActionBizEnum value();
+}
