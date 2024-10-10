e986d7e2d24f5b34cb85fb26c940b928f978c746
caixypromise <caixypromised@gmail.com>
2024-10-10 19:30:31 +0800

    feature: 新增默认支持序列化的实体类

---
+++ src/main/java/com/caixy/adminSystem/common/BaseSerializablePayload.java
@@ -0,0 +1,15 @@
+package com.caixy.adminSystem.common;
+
+import java.io.Serializable;
+
+/**
+ * 序列化实体基类
+ *
+ * @Author CAIXYPROMISE
+ * @name com.caixy.adminSystem.common.BaseSerializablePayload
+ * @since 2024/10/10 下午4:58
+ */
+public class BaseSerializablePayload implements Serializable
+{
+    private static final long serialVersionUID = 1L;
+}
