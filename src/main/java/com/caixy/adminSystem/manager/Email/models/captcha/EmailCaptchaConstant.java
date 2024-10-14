f271888fa6fdc5d9514c7c092faf31fe3643a746
caixypromise <caixypromised@gmail.com>
2024-10-15 00:50:58 +0800

    chore: 提交邮箱验证码常量

---
+++ src/main/java/com/caixy/adminSystem/manager/Email/models/captcha/EmailCaptchaConstant.java
@@ -0,0 +1,14 @@
+package com.caixy.adminSystem.manager.Email.models.captcha;
+
+/**
+ * 邮箱验证码常量
+ *
+ * @Author CAIXYPROMISE
+ * @name com.caixy.adminSystem.manager.Email.models.captcha.EmailCaptchaConstant
+ * @since 2024/10/10 下午10:19
+ */
+public interface EmailCaptchaConstant
+{
+    String CACHE_KEY_CODE = "code";
+
+}
