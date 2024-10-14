3dcc4e9767252e5e13d3a060cc52e265c2cf27bf
caixypromise <caixypromised@gmail.com>
2024-10-15 00:51:29 +0800

    feat: 新增基于Spring的全局校验异常处理器

--- src/main/java/com/caixy/adminSystem/exception/GlobalExceptionHandler.java
+++ src/main/java/com/caixy/adminSystem/exception/GlobalExceptionHandler.java
@@ -4,8 +4,13 @@ import com.caixy.adminSystem.common.BaseResponse;
 import com.caixy.adminSystem.common.ErrorCode;
 import com.caixy.adminSystem.common.ResultUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 全局异常处理器
@@ -27,5 +32,21 @@ public class GlobalExceptionHandler
     {
         log.error("RuntimeException", e);
         return ResultUtils.error(ErrorCode.SYSTEM_ERROR, "系统错误");
+    }
+
+    // 处理校验异常
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public BaseResponse<?> handleValidationExceptions(MethodArgumentNotValidException ex)
+    {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) ->
+        {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        log.info("参数校验异常: {}", errors);
+        // 返回自定义的响应体和状态码
+        return ResultUtils.error(ErrorCode.PARAMS_ERROR, "参数错误");
     }
 }
