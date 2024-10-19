dd0f38d3b4b7e04896c14a7793d648e321a4c225
caixypromise <caixypromised@gmail.com>
2024-10-20 01:40:43 +0800

    feat: mybatis新增支持乐观锁、防全表更新/删除操作

--- src/main/java/com/caixy/adminSystem/config/MyBatisPlusConfig.java
+++ src/main/java/com/caixy/adminSystem/config/MyBatisPlusConfig.java
@@ -2,6 +2,8 @@ package com.caixy.adminSystem.config;
 
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.BlockAttackInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
@@ -26,7 +28,11 @@ public class MyBatisPlusConfig
     public MybatisPlusInterceptor mybatisPlusInterceptor()
     {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
-        // 分页插件
+        //乐观锁插件
+        interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
+        //防全表更新与删除插件
+        interceptor.addInnerInterceptor(new BlockAttackInnerInterceptor());
+        //分页插件
         interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
         return interceptor;
     }
