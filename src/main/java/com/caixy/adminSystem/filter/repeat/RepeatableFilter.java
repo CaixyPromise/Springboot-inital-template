96d2c980cc7cc062e7afdc6a8a1280c9968fe83a
caixypromise <caixypromised@gmail.com>
2024-10-26 01:57:08 +0800

    feat: 增加线程池工具类，目前支持异步任务、支持偷窃任务的ForkJoinPool任务队列线程池

---
+++ src/main/java/com/caixy/adminSystem/filter/repeat/RepeatableFilter.java
@@ -0,0 +1,50 @@
+package com.caixy.adminSystem.filter.repeat;
+
+import com.caixy.adminSystem.utils.StringUtils;
+import org.springframework.http.MediaType;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * 解决在处理 HTTP 请求时，无法重复读取请求体（Request Body）的问题
+ *
+ * @Author CAIXYPROMISE
+ * @name com.caixy.adminSystem.filter.repeat.RepeatableFilter
+ * @since 2024/10/20 01:29
+ */
+public class RepeatableFilter implements Filter
+{
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException
+    {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException
+    {
+        ServletRequest requestWrapper = null;
+        if (request instanceof HttpServletRequest
+            && StringUtils.startsWithIgnoreCase(request.getContentType(), MediaType.APPLICATION_JSON_VALUE))
+        {
+            requestWrapper = new RepeatedlyRequestWrapper((HttpServletRequest) request, response);
+        }
+        if (null == requestWrapper)
+        {
+            chain.doFilter(request, response);
+        }
+        else
+        {
+            chain.doFilter(requestWrapper, response);
+        }
+    }
+
+    @Override
+    public void destroy()
+    {
+
+    }
+}
